1f2124f5c6e1f2a6edb430921d79051023472fb7
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-04-22 21:05:29 -0300

    feat: estrutura inicial do projeto, criação do componente Home e organização das pastas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./Components/Pages/Home";
+
+export default function App() {
   return (
     <div className="App">
-      
+      <Router>
+         <ul>
+          <li>Home</li>
+          <li>Contato</li>
+         </ul> 
+         <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/company' element={<Company/>}/>
+            <Route path='/contato' element={<Contato/>}/>
+            <Route path='/newproject' element={<NewProject/>}/>
+         </Routes>
+      </Router>
     </div>
   );
 }
 
-export default App;
