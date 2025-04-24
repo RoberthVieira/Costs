255e3ab64e3d5760728898a0f6febebde5b6cd38
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-04-23 22:13:33 -0300

    feat: Criação componentes e arquivos do layout, estilização do componente Container

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import Home from "./Components/Pages/Home";
+import Company from "./Components/Pages/Company";
+import Contato from "./Components/Pages/Contato";
+import NewProject from "./Components/Pages/NewProject";
+
+import Container from "./Components/layout/Container";
 
 export default function App() {
   return (
     <div className="App">
       <Router>
-         <ul>
-          <li>Home</li>
-          <li>Contato</li>
+        <ul>
+            <Link to="/">Sobre</Link>
+            <Link to="/company">Empresa</Link>
+            <Link to="/contato">Contato</Link>
+            <Link to="/newproject">Novo Projeto</Link>
          </ul> 
-         <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/company' element={<Company/>}/>
-            <Route path='/contato' element={<Contato/>}/>
-            <Route path='/newproject' element={<NewProject/>}/>
-         </Routes>
+          <Container customClass="minheight">
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/company' element={<Company/>}/>
+              <Route path='/contato' element={<Contato/>}/>
+              <Route path='/newproject' element={<NewProject/>}/>
+            </Routes>
+          </Container>
+          <p>Footer</p>
       </Router>
     </div>
   );
---
+++ src/Components/Pages/NewProject.js
@@ -0,0 +1,7 @@
+export default function NewProject(){
+    return(
+        <div>
+            <h1>NewProject</h1>
+        </div>
+    )
+}
