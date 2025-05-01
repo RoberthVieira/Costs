1e9a7abc9115964e36415e6a47004c444d565951
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-04-30 21:58:35 -0300

    feat: Adicionado estilos ao componente Projects. Adicionado LinkBtn para navegação à página de criação de projetos

--- src/Components/Pages/Projects.js
+++ src/Components/Pages/Projects.js
@@ -1,6 +1,10 @@
-import { useLocation } from "react-router-dom"
+import { useLocation } from "react-router-dom";
 
-import Message from "../layout/Message"
+import style from "./Projects.module.css";
+
+import Message from "../layout/Message";
+import Container from "../layout/Container";
+import LinkBtn from "../layout/LinkBtn";
 
 export default function Projects({}){
 
@@ -11,9 +15,15 @@ export default function Projects({}){
     }
 
     return (
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={style.project_container}>
+            <div className={style.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkBtn text='Criar Projetos' to='/newproject'/>
+            </div>
             {message && (<Message type='success' msg={message}/>)}
+            <Container customClass='start'>
+                <p>Projetos...</p>
+            </Container>
         </div>
     )
 }
