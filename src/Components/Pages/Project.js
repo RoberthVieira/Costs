80889994da9a3c991501c20774cc4f9a125a53a4
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-05-04 23:57:22 -0300

    Adiciona página de detalhes do projeto com edição e layout responsivo

--- src/Components/Pages/Project.js
+++ src/Components/Pages/Project.js
@@ -1,27 +1,68 @@
 import styles from './Project.module.css';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import Loading from './../layout/Loading';
+import Container from '../layout/Container';
 
 export default function Project() {
 
     const { id } = useParams()
 
     const [project, setProject] = useState([]);
+    const [showProjectForm, setShowProjectForm] = useState(false);
 
     useEffect(() => {
-        fetch(`http://localhost:5000/projects/${id}`, {
-            method: "GET",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        }).then(resposta => resposta.json())
-        .then((data) => {
-            setProject(data)
-        })
-        .catch(erro => console.log(erro))
-    })
+        setTimeout(() => {
+            fetch(`http://localhost:5000/projects/${id}`, {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }).then(resposta => resposta.json())
+            .then((data) => {
+                setProject(data)
+            })
+            .catch(erro => console.log(erro))
+        },400)
+    }, [id])
+
+    function toggleProjectForm() {
+        setShowProjectForm(!showProjectForm);
+    }
 
     return(
-        <p>{project.name}</p>
+        <>
+            {project.name ? (
+                <div className={styles.projectDetails}>
+                    <Container customClass="column">
+                        <div className={styles.detailsContainer}>
+                            <h1>Projeto: {project.name}</h1>
+                            <button onClick={toggleProjectForm} className={styles.btn}>
+                                {!showProjectForm ? 'Editar Projeto' : 'Fechar'}
+                            </button>
+                            {!showProjectForm ? (
+                                <div className={styles.projectInfo}>
+                                    <p>
+                                        <span>Categoria:</span> {project.categories.name}
+                                    </p>
+                                    <p>
+                                        <span>Total de Orçamento:</span> R${project.budget}
+                                    </p>
+                                    <p>
+                                        <span>Total Utilizado:</span> R${project.cost}
+                                    </p>
+                                </div>
+                            ) : (
+                                <div className={styles.projectInfo}>
+                                    <p>form</p>
+                                </div>
+                            )}
+                        </div>
+                    </Container>
+                </div>
+            ) : (
+                <Loading/>
+            )}
+        </>
     )
 }
