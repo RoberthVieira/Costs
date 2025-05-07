94d0971ab39e0d7867e712a081b6e3429fe78215
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-05-06 22:55:10 -0300

    feat: Adiciona o menu hamburguer em telas menores.

--- src/Components/layout/NavBar.js
+++ src/Components/layout/NavBar.js
@@ -1,29 +1,41 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 import styles from './NavBar.module.css';
 import Logo from '../../IMG/costs_logo.png';
 
 import Container from "./Container";
+import { FaBars, FaTimes } from "react-icons/fa"
 
 export default function NavBar(){
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    function toggleMenu(){
+        setMenuOpen(!menuOpen)
+    }
+
     return(
         <nav className={styles.navbar}>
             <Container className={styles.navbarContainer}>
                 <Link to='/'>
                     <img src={Logo} alt="Costs" />
                 </Link>
-                <ul className={styles.list}>
+                <div className={styles.menuIcon} onClick={toggleMenu}>
+                    {menuOpen ? <FaTimes/> : <FaBars/>}
+                </div>
+                <ul className={`${styles.list} ${menuOpen ? styles.active : ''}`}>
                     <li className={styles.item}>
-                        <Link to="/" className={styles.links}>Sobre</Link>
+                        <Link to="/" className={styles.links} onClick={() => setMenuOpen(false)}>Sobre</Link>
                     </li>
                     <li className={styles.item}>
-                        <Link to="/projects" className={styles.links}>Projetos</Link>
+                        <Link to="/projects" className={styles.links} onClick={() => setMenuOpen(false)}>Projetos</Link>
                     </li>
                     <li className={styles.item}>
-                        <Link to="/company"  className={styles.links}>Empresa</Link>
+                        <Link to="/company"  className={styles.links} onClick={() => setMenuOpen(false)}>Empresa</Link>
                     </li>
                     <li className={styles.item}>
-                        <Link to="/contato"  className={styles.links}>Contato</Link>
+                        <Link to="/contato"  className={styles.links} onClick={() => setMenuOpen(false)}>Contato</Link>
                     </li>
                 </ul>
             </Container>
